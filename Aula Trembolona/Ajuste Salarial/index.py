0f91d35b5979e74079327676af8d62980146c321
guilhrmz <152076235+guilhrmz@users.noreply.github.com>
2024-03-26 09:07:57 -0300

    Delete Aula Trembolona directory

--- Aula Trembolona/Ajuste Salarial/index.py
+++
@@ -1,9 +0,0 @@
-print("--------------------------")
-print("     Ajuste Salarial      ")
-print("--------------------------")
-
-print("Seu salario é de R$ 1.412")
-Porcentagem = int(input("Qual é o percentual de aumento salarial? (Não use %): "))
-SalarioMin = int(1412)
-SalarioNew = int((100 + Porcentagem) / 100 * 1412)
-print(f"Seu novo salario é {SalarioNew}")
