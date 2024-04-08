93186e30c74172af202920645543de9fcc0c0885
guilhrmz <152076235+guilhrmz@users.noreply.github.com>
2024-04-08 10:40:57 -0300

    Add files via upload

---
+++ Aula-Trembolona/Twofer/Twofer.py
@@ -0,0 +1,16 @@
+print("---------------------")
+print("       Twofer        ")
+print("---------------------")
+
+def Grande():
+    nome = input("Me informe seu Nome")
+
+    if not nome:
+        nome = ("voce")
+    else:
+        pass
+
+    print(F"um pra {nome},um pra mim")
+
+Grande()
+
